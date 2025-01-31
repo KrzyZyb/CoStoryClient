20044dc8d426a32e137b4e2cdc19ee8d70859efc
Krzysztof Zybul <krzysztof.zybul@god.dev>
2025-01-31 14:58:02 +0100

    Embedding terminal

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, OnDestroy, ViewEncapsulation} from '@angular/core';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import { AttachAddon } from 'xterm-addon-attach';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
   styleUrls: ['./terminal.component.css']
 })
-export class TerminalComponent implements OnInit {
-
-  constructor() { }
+export class TerminalComponent implements OnInit, OnDestroy {
+  @ViewChild('terminal', { static: true }) terminalDiv!: ElementRef;
+  private terminal!: Terminal;
+  private fitAddon!: FitAddon;
 
   ngOnInit(): void {
+    this.initializeTerminal();
   }
 
+  private initializeTerminal(): void {
+    this.terminal = new Terminal({
+      cursorBlink: true,
+      fontSize: 14,
+      theme: { background: '#1e1e1e' }
+    });
+
+    this.fitAddon = new FitAddon();
+    this.terminal.loadAddon(this.fitAddon);
+
+    this.terminal.open(this.terminalDiv.nativeElement);
+    this.fitAddon.fit();
+
+    this.terminal.writeln('Welcome to xterm.js in Angular!');
+    // const attachAddon = new AttachAddon(webSocket); TODO: Integrate with websocket
+
+    window.addEventListener('resize', () => this.fitAddon.fit());
+  }
+
+  ngOnDestroy(): void {
+    this.terminal.dispose();
+  }
 }
