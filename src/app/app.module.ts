42f367cea8c97b4cd4ccd38dc3947e677caa2d3d
Krzysztof Zybul <krzysztof.zybul@god.dev>
2025-03-11 12:47:28 +0100

    Navbar, visibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TerminalComponent } from './terminal/terminal.component';
     TerminalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
